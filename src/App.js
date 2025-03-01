3241ad008a08fae7479528cd0af6ccca5da09aed
Asingura Philip <asinguraphilip@gmail.com>
2025-03-01 19:02:59 +0300

    message function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './message.jsx';
 
 function App() {
+ 
+  const data = "words"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Message data={data}/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+    </>
+  
   );
 }
 
---
+++ src/message.jsx
@@ -0,0 +1,5 @@
+function Message(props){
+    return <p>Hello bro {props.data}</p>
+}
+
+export default Message
