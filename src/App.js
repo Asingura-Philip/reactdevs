7170ddaa5ea9396bcd4f98900340388da19e7e2f
Asingura Philip <asinguraphilip@gmail.com>
2025-03-03 18:19:52 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Message from './message.jsx';
 import Greet from './greeting.js';
 import Button from './button.js';
 import Card from './Card.js';
+import Userlist from './userList.js'
 
 function App() {
  
   const data = "words"
   // const name = "Louise"
+  const names = ['john ','james ','andrew ','peter ']
   return (
     <>
       <Message data={data}/>
@@ -27,6 +29,9 @@ function App() {
       <Card title={"Harry Potter"} description={"Magical school"}/>
       <Card title={"Hobit"} description={"Magical world"}/>
       <Card title={"Lord of the Rings"} description={"Dragons"}/>
+
+
+      <Userlist names={names}/>
     </>
 
   );
