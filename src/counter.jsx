bbb4a9d419b9734cd18f56e6f0f8bd8a697315d8
Asingura Philip <asinguraphilip@gmail.com>
2025-03-07 23:53:00 +0300

    counter file

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,1 +1,13 @@
 // 1)counter, 2)changes background color, 3)hides something with button click
+
+import React, {useState} from 'react';
+function myCounter(){
+    const[name,setName] = useState()
+    return(
+        <div>
+            <p></p>
+        </div>
+    )
+}
+
+export default myCounter
