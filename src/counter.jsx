70acf3da0e399fa2166c691a130f76506dbf7859
Asingura Philip <asinguraphilip@gmail.com>
2025-03-08 23:59:46 +0300

    cont

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,13 +1,18 @@
 // 1)counter, 2)changes background color, 3)hides something with button click
 
 import React, {useState} from 'react';
-function myCounter(){
-    const[name,setName] = useState()
+function MyCounter(){
+    const[number,setNumber] = useState(0);
+
+    const updateNumber =()=>{
+        setNumber(number + 1)
+    }
     return(
         <div>
-            <p></p>
+            <p>number : {number}</p>
+            <button onClick={updateNumber}>increase</button>
         </div>
     )
 }
 
-export default myCounter
+export default MyCounter
